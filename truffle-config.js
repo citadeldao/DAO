5df62f7e1952f2084e773e42bd6cf1267656e3ea
Pirat newTime <himyl@yandex.ru>
2021-06-15 03:37:53 -0700

    update quotes

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = fs.readFileSync('.secret').toString().trim();
 
 module.exports = {
   plugins: [
@@ -8,23 +8,23 @@ module.exports = {
   ],
   networks: {
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*", // Match any network id
+      network_id: '*', // Match any network id
       gas: 12721975
     },
     rinkeby: {
-      host: "localhost",
+      host: 'localhost',
       port: 8888,
-      network_id: "4", // Rinkeby ID 4
-      from: "0xF83B25aeC2360265Fb77C76ED0982AD6cE924FEB", // account from which to deploy
+      network_id: '4', // Rinkeby ID 4
+      from: '0xF83B25aeC2360265Fb77C76ED0982AD6cE924FEB', // account from which to deploy
       gas: 6712390
     },
     testnet: {
       provider: () => new HDWalletProvider({
         mnemonic,
         addressIndex: 0,
-        providerOrUrl: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+        providerOrUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545',
         chainId: 97,
       }),
       //provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
@@ -35,7 +35,7 @@ module.exports = {
       skipDryRun: true
     },
     bsc: {
-      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      provider: () => new HDWalletProvider(mnemonic, 'https://bsc-dataseed1.binance.org'),
       network_id: 56,
       confirmations: 10,
       timeoutBlocks: 200,
@@ -44,7 +44,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.6.2",
+      version: '0.6.2',
       settings: {
         optimizer: {
           enabled: true, // Default: false
